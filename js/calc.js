9fb10ea56d11aa85f92c87346f5b942a230a1b7e
Wojciech Schlesinger <wojciech.schlesinger@gmail.com>
2019-10-04 14:34:54 +0200

    CR2

---
+++ js/calc.js
@@ -0,0 +1,20 @@
+function calculateInsurance(){ 
+var name = document.getElementById("name").value;
+var age = Number(document.getElementById("age").value);
+var country = document.getElementById("country").value;
+var horsepower = Number(document.getElementById("horsepower").value);
+var insurance=0;
+var result= document.getElementById("result");
+switch(country){
+	case "Austria":
+		insurance = horsepower * 100 / age   + 50;
+		break;
+	case "Hungary":
+		insurance = horsepower * 120 / age  + 100;
+		break;
+	case "Greece":
+		insurance = horsepower * 150 / (age+3)  + 50;
+		break;
+}	
+	result.innerHTML = name+", your insurance costs: "+Math.round(insurance*100)/100+"€.";
+}
